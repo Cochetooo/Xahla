b09dad746bd9797a008779ea44757231c83aec11
ACOCHE <acochet@idfuse.fr>
2022-03-21 17:52:21 +0100

    UPD MapCollision fix collide

--- Physics/src/main/kotlin/MapCollision.kt
+++ Physics/src/main/kotlin/MapCollision.kt
@@ -1,11 +1,12 @@
 import org.joml.Vector2f
+import org.joml.Vector2i
 import org.joml.Vector3f
 import utils.LogLevel
 import utils.logger
 
 object MapCollision {
     /** Test if an object is set at a specific position in a 2D world. */
-    fun collideAt2D(position: Vector2f): Boolean {
+    fun collideAt2D(position: Vector2i): Boolean {
         if (!Physics.useCollisionGrid) {
             logger().internal_log("Trying to call collideAt2D() when collision grid is not set.",
                 LogLevel.WARNING, "MapCollision")
@@ -13,14 +14,15 @@ object MapCollision {
             return false
         }
 
-        if (Physics.collisionGrid[position.x][position.y][Physics.defaultZLayerFor2D] != 0)
+        if (Physics.collisionGrid!![position.x][position.y][Physics.defaultZLayerFor2D] != 0)
+            return true
 
         return false
     }
     /** Test if an object is set at a specific position in a 3D world. */
     fun collideAt3D(position: Vector3f): Boolean {
         if (!Physics.useCollisionGrid) {
-            logger().internal_log("Trying to call collideAt2D() when collision grid is not set.",
+            logger().internal_log("Trying to call collideAt3D() when collision grid is not set.",
                 LogLevel.WARNING, "MapCollision")
 
             return false
